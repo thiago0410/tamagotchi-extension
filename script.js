92633822e5391db1b8438518518c35acfd720fd0
Dudu <aplyiri@gmail.com>
2024-07-22 11:41:31 -0300

    barra mais ou mns

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ const btnAlimentar = document.getElementById('alimentar');
 const btnBrincar = document.getElementById('brincar');
 const btnLimpar = document.getElementById('limpar');
 
-btnAlimentar.addEventListener('click', alimentar());
-btnBrincar.addEventListener('click', brincar());
-btnLimpar.addEventListener('click', limpar());
+btnAlimentar.addEventListener('click', alimentar);
+btnBrincar.addEventListener('click', brincar);
+btnLimpar.addEventListener('click', limpar);
 
+function updateBars() {
+    updateBarColor('bar-fome-inner', fome, true);
+    updateBarColor('bar-felicidade-inner', felicidade, false);
+    updateBarColor('bar-limpeza-inner', limpeza, false);
+}
+
+function updateBarColor(barId, value, inverse) {
+    const barElement = document.getElementById(barId);
+    let percentage = inverse ? value * 10 : 100 - value * 10;
+    barElement.style.backgroundColor = percentage >= 50 ? 'red' : 'green';
+}
 
 function updateStatus() {
     document.getElementById('fome').innerText = fome;
     document.getElementById('felicidade').innerText = felicidade;
     document.getElementById('limpeza').innerText = limpeza;
+    updateBars();
 }
 
 function alimentar() {
@@ -39,10 +51,6 @@ function decreaseStatus() {
     felicidade = Math.max(felicidade - 1, 0);
     limpeza = Math.max(limpeza - 1, 0);
     updateStatus();
-
-    /*if (fome === 10 || felicidade === 0 || limpeza === 0) {
-        alert('Seu Tamagotchi está em mau estado!');
-    }*/
 }
 
 setInterval(decreaseStatus, 5000);
