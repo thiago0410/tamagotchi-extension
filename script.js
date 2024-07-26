1d91b2084815d1ddac1ce39fcc2e98f5f9cd497e
Dudu <aplyiri@gmail.com>
2024-07-26 08:03:11 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let fome = 0;
 let felicidade = 10;
 let limpeza = 10;
+ab = 100;
+ac = 100;
+ad = 0;
 
 const btnAlimentar = document.getElementById('alimentar');
 const btnBrincar = document.getElementById('brincar');
@@ -10,46 +13,58 @@ btnAlimentar.addEventListener('click', alimentar);
 btnBrincar.addEventListener('click', brincar);
 btnLimpar.addEventListener('click', limpar);
 
-function updateBars() {
-    updateBarColor('bar-fome-inner', fome, true);
-    updateBarColor('bar-felicidade-inner', felicidade, false);
-    updateBarColor('bar-limpeza-inner', limpeza, false);
-}
 
-function updateBarColor(barId, value, inverse) {
-    const barElement = document.getElementById(barId);
-    let percentage = inverse ? value * 10 : 100 - value * 10;
-    barElement.style.backgroundColor = percentage >= 50 ? 'red' : 'green';
-}
 
 function updateStatus() {
     document.getElementById('fome').innerText = fome;
     document.getElementById('felicidade').innerText = felicidade;
     document.getElementById('limpeza').innerText = limpeza;
-    updateBars();
 }
+
+document.querySelector('.barintL').style.height = ab + "%";
+document.querySelector('.barintB').style.height = ac + "%";
+document.querySelector('.barintF').style.height = ad + "%";
 
 function alimentar() {
     fome = Math.max(fome - 1, 0);
     felicidade = Math.min(felicidade + 1, 10);
     updateStatus();
+    document.querySelector('.barintF').style.height = ad + "%";
+    ad = ad - 10;
 }
 
 function brincar() {
     felicidade = Math.min(felicidade + 2, 10);
     limpeza = Math.max(limpeza - 1, 0);
     updateStatus();
+    ab = ab - 10;
+    ac = ac + 10;
+    ad = ad + 10;
+    document.querySelector('.barintL').style.height = ab + "%";
+    document.querySelector('.barintB').style.height = ac + "%";
+    document.querySelector('.barintF').style.height = ad + "%";
+    
 }
 
 function limpar() {
     limpeza = 10;
     updateStatus();
+    ab = 100;
+    document.querySelector('.barintL').style.height = ab + "%";
+    document.querySelector('.barintB').style.height = ac + "%";
+    document.querySelector('.barintF').style.height = ad + "%";
 }
 
 function decreaseStatus() {
     fome = Math.min(fome + 1, 10);
+    ad = ad + 10;
     felicidade = Math.max(felicidade - 1, 0);
+    ac = ac - 10;
     limpeza = Math.max(limpeza - 1, 0);
+    ab = ab - 10; 
+    document.querySelector('.barintL').style.height = ab + "%";
+    document.querySelector('.barintB').style.height = ac + "%";
+    document.querySelector('.barintF').style.height = ad + "%";
     updateStatus();
 }
 
