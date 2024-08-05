9b60d8cdb52cfc60ca65b03ef1db32e44d7d7367
thiago0410 <thiagonoronha1004@gmail.com>
2024-08-05 10:51:28 -0300

    js

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-let fome = 10;
-let felicidade = 10;
-let limpeza = 10;
-limpo = 100;
-feliz = 100;
-satisfeito = 100;
+/* localStorage 
+var estado = {
+  fome: 10, 
+  felicidade: 10,
+  limpeza : 10,
+  limpo: 100,
+  feliz: 100,
+  satisfeito: 100
+};*/
+
+var estado;
+
+function salvaEstado(){
+  const estadoJSON = JSON.stringify(estado);
+  localStorage.estado = estadoJSON;
+  console.log("Salva estado");
+}
+
+function recuperaEstado(){
+  const estadoRec =  localStorage.estado;
+  estado = JSON.parse(estadoRec);
+  console.log("Recupera estado");
+}
+
+window.onload = function(){
+  if (localStorage.estado){
+    recuperaEstado();
+  }
+  else {
+    var estado = {
+      fome: 10, 
+      felicidade: 10,
+      limpeza : 10,
+      limpo: 100,
+      feliz: 100,
+      satisfeito: 100
+    };
+  }
+};
+
 
 const btnAlimentar = document.getElementById('alimentar');
 const btnBrincar = document.getElementById('brincar');
 const btnLimpar = document.getElementById('limpar');
 
+/* GIF */
 const gif = document.getElementById("gif");
 const gifDuration = 12000;
 
@@ -24,55 +59,56 @@ function troca_gif() {
   }
 }
 
-document.querySelector('.barintLimpeza').style.width = limpo + "%";
-document.querySelector('.barintBrincar').style.width = feliz + "%";
-document.querySelector('.barintFome').style.width = satisfeito + "%";
+/*document.querySelector('.barintLimpeza').style.width = estado.limpo + "%";
+document.querySelector('.barintBrincar').style.width = estado.feliz + "%";
+document.querySelector('.barintFome').style.width = estado.satisfeito + "%";*/
 
 function barras() {
-  document.querySelector('.barintLimpeza').style.width = limpo + "%";
-  document.querySelector('.barintBrincar').style.width = feliz + "%";
-  document.querySelector('.barintFome').style.width = satisfeito + "%";
+  document.querySelector('.barintLimpeza').style.width = estado.limpo + "%";
+  document.querySelector('.barintBrincar').style.width = estado.feliz + "%";
+  document.querySelector('.barintFome').style.width = estado.satisfeito + "%";
+  salvaEstado();
 }
 
 //a barra de felicidade é autonoma
 // esse if do alimentar e brincar funciona como limitador, 
 function alimentar() {
-  fome = Math.max(fome + 1, 10);
-  felicidade = Math.max(felicidade + 1, 10);
-  document.querySelector('.barintFome').style.width = satisfeito + "%";
-  satisfeito = satisfeito + 10;
-  if(satisfeito > 100){
-    satisfeito = 100;
+  estado.fome = Math.max(estado.fome + 1, 10);
+  estado.felicidade = Math.max(estado.felicidade + 1, 10);
+  document.querySelector('.barintFome').style.width = estado.satisfeito + "%";
+  estado.satisfeito = estado.satisfeito + 10;
+  if(estado.satisfeito > 100){
+    estado.satisfeito = 100;
   }
-  console.log("alimentando:" + satisfeito);
+  console.log("alimentando:" + estado.satisfeito);
   barras();
 }
 
 function brincar() {
-  felicidade = Math.max(felicidade + 1, 10);
-  limpeza = Math.max(limpeza - 1, 0);
-  limpo = limpo - 10;
-  feliz = feliz + 10;
-  if(feliz > 100){
-    feliz = 100;
+  estado.felicidade = Math.max(estado.felicidade + 1, 10);
+  estado.limpeza = Math.max(estado.limpeza - 1, 0);
+  estado.limpo = estado.limpo - 10;
+  estado.feliz = estado.feliz + 10;
+  if(estado.feliz > 100){
+    estado.feliz = 100;
   }
-  console.log("FELICIDADE:" + feliz);
+  console.log("FELICIDADE:" + estado.feliz);
   barras();
 }
 
 function limpar() {
-  limpeza = 10;
-  limpo = 100;
+  estado.limpeza = 10;
+  estado.limpo = 100;
   barras();
 }
 
 function decreaseStatus() {
-  fome = Math.max(fome - 1, 10);
-  satisfeito = satisfeito - 10;
-  felicidade = Math.max(felicidade - 1, 0);
-  feliz = feliz - 10;
-  limpeza = Math.max(limpeza - 1, 0);
-  limpo = limpo - 10;
+  estado.fome = Math.max(estado.fome - 1, 10);
+  estado.satisfeito = estado.satisfeito - 10;
+  estado.felicidade = Math.max(estado.felicidade - 1, 0);
+  estado.feliz = estado.feliz - 10;
+  estado.limpeza = Math.max(estado.limpeza - 1, 0);
+  estado.limpo = estado.limpo - 10;
   console.log
   barras();
 }
